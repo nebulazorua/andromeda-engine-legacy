a3eedef3be176143ee0dbde0265ce1a86ed79f6d
HumanKR <kstv7777@gmail.com>
2022-06-26 01:17:40 +0900

    bug fix for alt note uwu

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3805,7 +3805,8 @@ class PlayState extends MusicBeatState
 					anim = 'hold${dir}End';
 			}
 		}
-		if(daNote.noteType=='alt')suffix+='-alt';
+		//if(daNote.noteType=='alt')suffix+='-alt';
+		//Just in case, I will annotate this code for the future. -Human
 
 		if(who.animation.getByName(anim+suffix)!=null)
 			anim += suffix;
